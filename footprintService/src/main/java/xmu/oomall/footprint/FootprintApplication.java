fce9f96b07c53bf17c537b2d94965874d557f0e6
buuuuuuug <44496732+buuuuuuug@users.noreply.github.com>
2019-12-21 00:00:35 +0800

    添加eureka

--- footprintService/src/main/java/xmu/oomall/footprint/FootprintApplication.java
+++ footprintService/src/main/java/xmu/oomall/footprint/FootprintApplication.java
@@ -3,9 +3,11 @@ package xmu.oomall.footprint;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
+@EnableEurekaClient
 public class FootprintApplication {
 
     public static void main(String[] args) {
